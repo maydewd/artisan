078e1f71625d46d5527c7e9a33f7f0254c42b252
Ryan St.Pierre <ras70@duke.edu>
2016-11-04 11:13:25 -0400

    Moved navigation and style out of index.ios, need to do it for Andriod

---
+++ mobile/helpers/Navigation.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import LoginScreen from '../Screens/LoginScreen.js'
+import NewPost from '../Screens/NewPost.js'
+import DiscoverPost from '../Screens/DiscoverPost.js'
+import StorkfrontPost from '../Screens/StorkfrontPost.js'
+import MainNavBar from '../Components/MainNavBar.js'
+import Discover from '../Screens/Discover.js'
+import DiscoverSettings from '../Screens/DiscoverSettings.js'
+import StorkFront from '../Screens/StorkFront.js'
+import MyBundle from '../Screens/MyBundle.js'
+import Messages from '../Screens/Messages.js'
+import BottomTabBar from '../Components/BottomTabBar.js'
+
+export function render(route, navigator) {
+  var routeId = route.id;
+  if (routeId === 'login') {
+    return (
+      <LoginScreen
+        navigator={navigator} />
+    );
+  }
+  if (routeId === 'mainView') {
+    return (
+      <BottomTabBar
+        navigator={navigator} />
+    );
+  }
+  if (routeId === 'discoverSettings') {
+    return (
+      <DiscoverSettings
+        navigator={navigator} />
+    );
+  }
+  if (routeId === 'myBundle') {
+    return (
+      <MyBundle
+        navigator={navigator} />
+    );
+  }
+  if (routeId === 'newPost') {
+    return (
+      <NewPost
+        navigator={navigator} />
+    );
+  }
+  if (routeId === 'discoverPost') {
+    return (
+      <DiscoverPost
+        item={route.item}
+        navigator={navigator} />
+    );
+  }
+  if (routeId === 'storkfrontPost') {
+    return (
+      <StorkfrontPost
+        item={route.item}
+        navigator={navigator} />
+    );
+  }
+  if (routeId === 'messages') {
+    return (
+      <Messages
+        creatorID = {route.creator}
+        navigator={navigator} />
+    );
+  }
+}
