8be6d7128db4e1463b45222c5c52071c5de193af
David Maydew <dmm68@duke.edu>
2016-11-05 16:23:39 -0400

    Implement Edit and Delete functionality

--- server/app/config/routes.js
+++ server/app/config/routes.js
@@ -40,10 +40,12 @@ module.exports = function (app, passport) {
 
   // API LISTING routes
   apiRoutes.post('/listings', [jwtAuth, upload.single('image')], listings.create);
-  apiRoutes.get('/listings', jwtAuth, listings.showPosts); 
+  apiRoutes.get('/listings', jwtAuth, listings.showPosts);
   apiRoutes.get('/listings/me', jwtAuth, listings.showMine);
   apiRoutes.get('/listings/liked', jwtAuth, listings.showLiked);
   apiRoutes.get('/listings/:listingID', jwtAuth, listings.show);
+  apiRoutes.post('/listings/:listingID', [jwtAuth, upload.single('image')], listings.edit);
+  apiRoutes.delete('/listings/:listingID', jwtAuth, listings.delete);
   apiRoutes.post('/listings/:listingID/like', jwtAuth, listings.like);
   apiRoutes.post('/listings/:listingID/unlike', jwtAuth, listings.unlike);
 
