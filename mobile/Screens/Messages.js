923c19d051ee5e6d389c81e05dbdd72cf5ba68da
Ryan St.Pierre <ras70@duke.edu>
2016-11-13 15:58:32 -0500

    Changed message icon from envelope to chat bubbles

--- mobile/Screens/Messages.js
+++ mobile/Screens/Messages.js
@@ -14,12 +14,23 @@ import {
   TouchableOpacity
 } from 'react-native';
 var NavigationBar = require('react-native-navbar');
-import {getScreenHeight, topNavBarHeight, getScreenWidth} from '../helpers/dimension'
+import {usableWithTop} from '../helpers/dimension'
 import { SwipeListView } from 'react-native-swipe-list-view';
 styles = require('../Styles/Layouts');
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionic from 'react-native-vector-icons/Ionicons';
 
 class Messages extends Component {
+
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(
+        [{buyer: "Buyer 1"}, {buyer: "Buyer 2"}]
+      ),
+    };
+  }
 
   render() {
     var titleConfig = {
@@ -38,11 +49,29 @@ class Messages extends Component {
         title={titleConfig}
         leftButton={leftButtonConfig}
         />
-        <Text>
-        Messages
-        </Text>
+        <ListView
+          style = {{height: usableWithTop()}}
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => this.renderRow(rowData)}
+        />
       </View>
     );
+  }
+
+  renderRow(rowData) {
+    var imagePath = "public/uploads/listings/loading.jpg"
+    return (
+      <View style = {{flexDirection: 'row'}}>
+        <Image style = {{height: 80, width: 80}}
+         source = {{uri: "http://colab-sbx-137.oit.duke.edu:3000/" + imagePath}}/>
+         <Text> {rowData.buyer} </Text>
+           <Ionic onPress= {() => this._mail(item)} name="ios-chatbubbles" size={30} color="black" />
+      </View>
+     );
+  }
+
+  mail(item) {
+    console.log("trying to message");
   }
 
   pop() {
